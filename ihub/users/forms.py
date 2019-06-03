b578d93dd83ff05305d081ef6624349165254615
iqDF <daniel.thekurniadi@gmail.com>
2019-06-03 14:00:00 +0100

    Users - Forms remove redundant password field

--- ihub/users/forms.py
+++ ihub/users/forms.py
@@ -4,4 +4,4 @@ from .models import User
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('name', 'email', 'matric', 'password')
+        fields = ('name', 'email', 'matric') # password is automatically added by UserCreationForm
