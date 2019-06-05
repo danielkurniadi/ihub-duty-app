68f0d1603cc3c5db67b55ae3a0286bd2f2c88dfe
iqDF <daniel.thekurniadi@gmail.com>
2019-06-05 18:56:19 +0800

    Users - Admin define user for django admin

--- ihub/users/admin.py
+++ ihub/users/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'matric', 'name', 'last_login')}),
+        ('Permissions', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser',
+            'groups',
+            'user_permissions',
+        )}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('name', 'email', 'matric', 'password1', 'password2')
+            }
+        ),
+    )
+
+    list_display = ('email', 'name', 'matric', 'is_staff', 'last_login')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    search_fields = ('email', 'matric')
+    ordering = ('email', 'last_login')
+    filter_horizontal = ('groups', 'user_permissions',)
+
+
+admin.site.register(User, UserAdmin)
