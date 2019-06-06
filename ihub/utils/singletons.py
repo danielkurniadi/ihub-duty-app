7f4b168ed8e5f0ed426968640c603fed57cc82e8
iqDF <daniel.thekurniadi@gmail.com>
2019-06-06 14:00:00 +0100

    Utility - Singletons base singleton

---
+++ ihub/utils/singletons.py
@@ -0,0 +1,27 @@
+from django.db.models import models
+
+class Singleton(object):
+    instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.instance:
+            return cls.instance
+        else:
+            cls.instance = object.__new__(cls, *args, **kwargs)
+            return cls.instance
+
+class SingletonModel(models.Model):
+	class Meta:
+		abstract = True
+
+	def save(self, *args, **kwargs):
+		self.pk = 1
+		super(SingletonModel, self).save(*args, **kwargs)
+
+	def delete(self, *args, **kwargs):
+		pass
+
+	@classmethod
+	def load(cls):
+		obj, created = cls.objects.get_or_create(pk=1)
+		return obj
