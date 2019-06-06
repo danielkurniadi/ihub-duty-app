113b0cd609684396a8dc1c87bff6df273bef5ea7
iqDF <daniel.thekurniadi@gmail.com>
2019-06-06 14:00:00 +0100

    Duties - Admin register Duty model

--- ihub/duties/admin.py
+++ ihub/duties/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Duty
 
-# Register your models here.
+class DutyAdmin(admin.ModelAdmin):
+    model = Duty
+
+admin.site.register(Duty, DutyAdmin)
