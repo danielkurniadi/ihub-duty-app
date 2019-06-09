b45d3f18ab431cf78cd8a8dc0d8662f0be3b8eb9
iqDF <daniel.thekurniadi@gmail.com>
2019-06-09 15:10:34 +0800

    Duties - Tests duty manager basics duty functionalities

--- ihub/duties/tests/test_model_manager.py
+++ ihub/duties/tests/test_model_manager.py
@@ -22,25 +22,111 @@ class DutyManagerTests(TestCase, RandomSupport):
         """Test given a user, manager is able to start a duty with that user.
         Foreign key relations are formed between user-duty and duty-manager.
         """
-        pass
+        # Step 1: initialise user & duty manager
+        user = self.generate_ihub_user()
+        duty_manager = DutyManager.load()
+
+        # Step 1b: verify duty is empty initially
+        self.assertEqual(user.duty_set.count(), 0)
+        self.assertEqual(duty_manager.active_duties.count(), 0) 
+
+        # Step 2: create duty with user using duty manager
+        duty = duty_manager.start_duty(user)
+
+        # Step 3: verify duty is created for user by duty manager
+        self.assertEqual(user.duty_set.count(), 1)
+        self.assertEqual(duty_manager.active_duties.count(), 1)
+
+        # Step 3b: verify user associated to duty in duty manager
+        self.assertEqual(duty, duty_manager.get_duties_of(user)[0])
 
     def test_manager_start_duty_with_debtee(self):
         """Test given a user buying duty debt from debtee, manager is able to
         start a duty with foreign key to user and to debtee. 
         """
-        pass
+        # Step 1: initialise user, debtee, and duty manager
+        user = self.generate_ihub_user()
+        debtee = self.generate_ihub_user()
+        duty_manager = DutyManager.load()
+
+        # Step 1b: verify duty is empty initially
+        self.assertEqual(user.duty_set.count(), 0)
+        self.assertEqual(duty_manager.active_duties.count(), 0)
+        self.assertEqual(debtee.duty_debt_set.count(), 0)
+
+        # Step 2: create duty debt with user and debtee via duty manager
+        duty = duty_manager.start_duty(user, debtee=debtee)
+
+        # Step 3: verify duty debt is created for debtee in duty manager
+        self.assertEqual(user.duty_set.count(), 1)
+        self.assertEqual(duty_manager.active_duties.count(), 1)
+        self.assertEqual(debtee.duty_debt_set.count(), 1)
+
+        # Step 3b: verify duty started is associated as debt to debtee
+        self.assertIn(duty, debtee.duty_debt_set.all())
+        self.assertTrue(debtee.duty_debt_set.filter(pk=duty.id).exists())
 
     def test_manager_filter_finished_duties(self):
         """Test given several duties which some has been finished, manager is able to
         filter finished duties.
         """
-        pass
-    
+        # Step 1: initialise 4 duties
+        duty1 = Duty.objects.create()
+        duty2 = Duty.objects.create()
+        duty3 = Duty.objects.create()
+        duty4 = Duty.objects.create()
+        duty_manager = DutyManager.load()
+
+        # Step 1b: associate duties with duty manager as active duties
+        duty_manager.active_duties.add(duty1, duty2, duty3, duty4,
+            bulk=True)
+        
+        # Step 2: mark duty1 & duty2 as finished
+        duty1.force_finish_duty()
+        duty2.force_finish_duty()
+
+        # Step 3: manager filter finished duty
+        finished_duties = duty_manager.filter_finished_duties()
+        
+        # Step 3b: verify finished duties are filtered
+        self.assertEqual(finished_duties.count(), 2)
+        self.assertIn(duty1, finished_duties)
+        self.assertIn(duty2, finished_duties)
+
+        # duty_manager.active_duties.remove(*finished_duties, bulk=True)
+        # self.assertEqual(duty_manager.active_duties.count(), 2)
+
     def test_manager_remove_finished_duties(self):
         """Test given several duties which some has been finished, manager is able to
         filter and remove the unfinished duties.
         """
-        pass
+        # Step 1: initialise 4 duties
+        duty1 = Duty.objects.create()
+        duty2 = Duty.objects.create()
+        duty3 = Duty.objects.create()
+        duty4 = Duty.objects.create()
+        duty_manager = DutyManager.load()
+
+        # Step 1b: associate duties with duty manager as active duties
+        duty_manager.active_duties.add(duty1, duty2, duty3, duty4,
+            bulk=True)
+        
+        # Step 2: mark duty1 & duty2 as finished
+        duty1.force_finish_duty()
+        duty2.force_finish_duty()
+
+        # Step 3: manager filter finished duty
+        finished_duties = duty_manager.remove_finished_duties()
+
+        # Step 3b: verify finished duties are removed
+        self.assertEqual(finished_duties.count(), 2)
+        self.assertIn(duty1, finished_duties)
+        self.assertIn(duty2, finished_duties)
+        self.assertEqual(duty_manager.active_duties.count(), 2)
+
+        # Step 3c: verify unfinished duties are still associated with duty manager
+        self.assertIn(duty3, duty_manager.active_duties.all())
+        self.assertIn(duty4, duty_manager.active_duties.all())
 
     #########################################################################################
 
