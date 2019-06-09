9aa0d55a1e96b24d57436b7ac1427579d36cd1bf
iqDF <daniel.thekurniadi@gmail.com>
2019-06-09 15:12:09 +0800

    Duties - Model duty manager add debtee param to start duty

--- ihub/duties/models/duty_manager.py
+++ ihub/duties/models/duty_manager.py
@@ -10,60 +10,62 @@ from duties.errors import MaxDutyCountError
 User = get_user_model()
 
 class DutyManager(SingletonModel):
-    MAX_DUTY = 1
+	MAX_DUTY = 1
 
-    ########################################
-    # Active Duties
-    ########################################
+	########################################
+	# Active Duties
+	########################################
 
-    def start_duty(self, user, debtee=None):
-        # check MAX_DUTY threshold condition
-        if self.duties_count() < DutyManager.MAX_DUTY:
-           duty = self.active_duties.create()
-           user.duty_set.add(duty)
-           user.save(); duty.save()
-           return duty
-        # MAX_DUTY condition fail, raise error
-        raise MaxDutyCountError
+	def start_duty(self, user, debtee=None):
+		# check MAX_DUTY threshold condition
+		if self.active_duties.count() < DutyManager.MAX_DUTY:
+			duty = self.active_duties.create()
+			user.duty_set.add(duty)
+			if debtee:
+				debtee.duty_debt_set.add(duty)
+				debtee.save()
+			user.save(); duty.save()
+			return duty
+		# MAX_DUTY condition fail, raise error
+		raise MaxDutyCountError
 
-    def filter_finished_duties(self):
-        # filter duties which end has passed
-        finished_duties = self.active_duties.filter(duty_end__lte=timezone.now())
-        return finished_duties
+	def filter_finished_duties(self):
+		# filter duties which end has passed
+		finished_duties = self.active_duties.filter(duty_end__lte=timezone.now())
+		return finished_duties
 
-    def remove_finished_duties(self):
-        # filter duties which end has passed
-        finished_duties = tuple(
-            duty for duty in self.filter_finished_duties()
-        )
-        # remove all relationships to finished duties w/o deleting object
-        if finished_duties:
-            self.active_duties.remove(*finished_duties, bulk=True)
-        return finished_duties # tuple
+	def remove_finished_duties(self):
+		# filter duties which end has passed
+		finished_duties = self.filter_finished_duties()
+		# remove all relationships to finished duties w/o deleting object
+		if finished_duties:
+			self.active_duties.remove(*finished_duties, bulk=True)
+		return finished_duties # tuple
 
-    def get_duties_of(self, user):
-        user_active_duties = tuple(
-            duty for duty in self.active_duties.filter(user__email=user.email)
-        )
-        return user_active_duties # tuple
+	def get_duties_of(self, user):
+		user_active_duties = tuple(
+			duty for duty in self.active_duties.filter(user__email=user.email)
+		)
+		return user_active_duties # tuple
 
-    def remove_duties_of(self, user):
-        if user.duty_set.count() == 0:
-            return tuple()
-        user_active_duties = self.get_duties_of(user)
-        # remove if any duty of user in manager 
-        if user_active_duties:
-            self.active_duties.remove(*user_active_duties, bulk=True)
-        return user_active_duties # tuple
+	def remove_duties_of(self, user):
+		# check defensively if user ever has duty
+		if user.duty_set.count() == 0:
+			return tuple()
+		user_active_duties = self.get_duties_of(user)
+		# remove if any duty of user in manager 
+		if user_active_duties:
+			self.active_duties.remove(*user_active_duties, bulk=True)
+		return user_active_duties # tuple
 
-    ########################################
-    # Onduty User
-    ########################################
+	########################################
+	# Onduty User
+	########################################
 
-    def get_onduty_users(self):
-        onduty_users = self.active_duties.values_list('user')
-        return onduty_users
+	def get_onduty_users(self):
+		onduty_users = self.active_duties.values_list('user')
+		return onduty_users
 
-    def is_onduty(self, user):
-        onduty_users = self.get_onduty_users()
-        return (user in onduty_users)
+	def is_onduty(self, user):
+		onduty_users = self.get_onduty_users()
+		return (user in onduty_users)
