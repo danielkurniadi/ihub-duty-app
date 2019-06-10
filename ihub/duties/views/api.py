f8873c4a20cc5cf85db51882eab48127df8cb677
iqDF <daniel.thekurniadi@gmail.com>
2019-06-10 21:19:56 +0800

    Duties - Views minor refactoring logics and import

--- ihub/duties/views/api.py
+++ ihub/duties/views/api.py
@@ -4,14 +4,19 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-from utils.singletons import SingletonModel
-from duties.errors import (
+from duties.models import (
+    Duty, DutyManager
+)
+from duties.serializers import DutySerializer
+from users.serializers import UserSerializer
+from bridge.constants.errors import (
 	MaxDutyCountError, UnfinishedDutyError
 )
 
@@ -21,11 +26,21 @@ duty_manager = DutyManager.load()
 @api_view(['POST'])
 @permission_classes((IsAuthenticated, ))
 def duty_api_start_view(request):
+    # preprocessing
+    duty_manager.remove_finished_duties()
+
     user = request.user
+    debtee = None
+
+    # check debtee data if specified
+    debtee_params = request.data.get('debtee')
+    if debtee_params:
+        debtee = get_object_or_404(User, **debtee_params)
+
+    # try start duty
     try:
-        duty_manager.remove_finished_duties()
-        duty = duty_manager.start_duty(user)
-    except UnfinishedDutyError as e:
+        duty = duty_manager.start_duty(user, debtee=debtee)
+    except (UnfinishedDutyError,MaxDutyCountError) as e:
         return Response(
             {
                 'success': False,
@@ -33,23 +48,7 @@ def duty_api_start_view(request):
             },
             status=status.HTTP_400_BAD_REQUEST 
         )
-    except MaxDutyCountError as e:
-        return Response(
-            {
-                'success': False,
-                'message': e.message,
-            },
-            status=status.HTTP_400_BAD_REQUEST
-        )
-    except Exception as e:
-        return Response(
-            {
-                'success': False,
-                'message': e.message,
-            },
-            status=status.HTTP_500_INTERNAL_SERVER_ERROR
-        )
-    
+
     # Success
     serializer = DutySerializer(duty)
     return Response(
@@ -65,32 +64,26 @@ def duty_api_start_view(request):
 @permission_classes((IsAuthenticated, ))
 def duty_api_detail_view(request):
     user = request.user
+
+    # preprocessing
     duty_manager.remove_finished_duties()
 
-    if user.duty_set.count() == 0:
+    if (user.duty_set.count() == 0) or (duty_manager.is_onduty(user) == False):
         return Response(
-            {
-                'success': False, 
-                'message': "User has no ongoing duty at the moment."
-            },
-            status=status.HTTP_400_BAD_REQUEST
-        )
-    elif duty_manager.is_onduty(user) == False:
-        return return Response(
             {
                 'success': False, 
                 'message': "User's duty is not registered in manager"
             },
             status=status.HTTP_400_BAD_REQUEST
         )
-    
+
     # Success
-    duty = duty_manager.get_duties_of(user)
-    serializer = DutySerializer(duty, many=True)
+    duties = duty_manager.get_duties_of(user)
+    serializer = DutySerializer(duties, many=True)
     return Response(
         {
             'success': True,
-            'message': "%s sent" % duty,
+            'message': "Duties sent. MAX_DUTY: %d" % DutyManager.MAX_DUTY,
             'payload': serializer.data
         },
         status=status.HTTP_200_OK
