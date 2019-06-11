e56f6de4548927152c1d960fcfa2c4db09910680
iqDF <daniel.thekurniadi@gmail.com>
2019-06-11 21:46:30 +0800

    Duties - Urls add path for page view

--- ihub/duties/urls.py
+++ ihub/duties/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import (
-    duty_api_start_view, duty_api_detail_view
+    duty_api_start_view, duty_api_detail_view,
+    duty_template_view
 )
 
 app_name = 'duties'
+
+page_urlpath = [
+    path('page', duty_template_view, name='duty-page'),
+]
 
 api_urlpath = [
     path('create/', duty_api_start_view, name='duty-create'),
@@ -12,6 +17,6 @@ api_urlpath = [
 ]
 
 urlpatterns = [
-    # path('', include(page_urlpath)),
+    path('', include(page_urlpath)),
     path('api/', include(api_urlpath)),
 ]
