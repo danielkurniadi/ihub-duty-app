5ed1f03601dd86cce85f1001fc86506dc6502e7f
iqDF <daniel.thekurniadi@gmail.com>
2019-06-11 23:09:37 +0800

    Duties - Views add logic check onduty user

--- ihub/duties/views/pages.py
+++ ihub/duties/views/pages.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponseNotAllowed
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
@@ -18,4 +19,9 @@ def duty_template_view(request):
     """Render duty page for on-duty or idle user
     """
     if request.method == 'GET':
-        return render(request, 'duty.html')
+        user = request.user
+        if duty_manager.is_onduty(user):
+            return render(request, 'onduty.html')
+        return render(request, 'getstarted.html')
+    else:
+        return HttpResponseNotAllowed("Wrong Method")
